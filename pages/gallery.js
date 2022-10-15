d8af03b677ab586a1f4a41997b301ccb05913435
tawakalmit <tawakal.mit@gmail.com>
2022-10-15 19:59:11 +0700

    gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,7 +1,12 @@
 import Carousel from "../components/Carousel"
 import Layout from "../components/Layout"
+import { useState } from "react";
+import Image from "next/image";
 
 export default function Gallery() {
+    const [gambar, setGambar] = useState([
+        {id: 1},{id: 2},{id: 3},{id: 4},{id: 5},{id: 6},{id: 7},{id: 8},{id: 9},{id: 10},{id: 11},{id: 12},{id: 13},{id: 14},{id: 15},{id: 16},{id: 17},{id: 18},{id: 19},{id: 20},{id: 21},{id: 23},{id: 24},{id: 25},{id: 26},{id: 27},{id: 28},{id: 29},{id: 30},{id: 31},{id: 32},{id: 33},{id: 34},{id: 35},{id: 36},{id: 37},{id: 38},{id: 39},{id: 40},{id: 41},
+      ],);
   return (
     <>
     <Layout 
@@ -10,6 +15,12 @@ export default function Gallery() {
 
         <h1 className="text-3xl font-bold text-center md:text-left">Gallery</h1>
         <Carousel />
+
+        <div className="w-11/12 mx-auto flex justify-evenly items-center flex-wrap mt-5">
+            
+            {gambar.map((data) => (<div className="m-2"><Image src={`/carousel/carousel (${data.id}).jpeg`} width={500} height={380} className="rounded-xl" /></div>))}
+            
+        </div>
 
         </div>
     }
