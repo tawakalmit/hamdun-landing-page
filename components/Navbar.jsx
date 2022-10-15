05c0fc77e50dba2d0e69cf7024481a4dde89985d
tawakalmit <tawakal.mit@gmail.com>
2022-10-15 13:21:25 +0700

    fix links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
 import { FiMenu } from "react-icons/fi"
+import { useRouter } from 'next/router'
 
 export default function Navbar() {
+
+  const router = useRouter();
+
   return (
     <>
     <nav className='w-full h-[4rem] bg-[#25274C]'>
         <div className='w-10/12 mx-auto h-[4rem] flex items-center justify-between'>
-            <a href='/' className='text-white font-semibold text-3xl'>Hamdun</a>
+            <p onClick={(e) => router.push('/') } className='cursor-pointer text-white font-semibold text-3xl'>Hamdun</p>
             <FiMenu size={30} color="white" className='md:hidden' />
             <div className='w-[30rem] hidden md:flex justify-between'>
-                <a href="/" className='text-white hover:opacity-60'>Beranda</a>
-                <a href="/jadwal" className='text-white hover:opacity-60'>Jadwal</a>
-                <a href="/artikel" className='text-white hover:opacity-60'>Artikel</a>
-                <a href="/berita" className='text-white hover:opacity-60'>Berita</a>
-                <a href="/gallery" className='text-white hover:opacity-60'>Gallery</a>
-                <a href="/pustaka" className='text-white hover:opacity-60'>Pustaka</a>
+                <p onClick={(e) => router.push('/') } className='text-white hover:opacity-60'>Beranda</p>
+                <p onClick={(e) => router.push('/jadwal') } className='text-white hover:opacity-60'>Jadwal</p>
+                <p onClick={(e) => router.push('/artikel') } className='text-white hover:opacity-60'>Artikel</p>
+                <p onClick={(e) => router.push('/berita') } className='text-white hover:opacity-60'>Berita</p>
+                <p onClick={(e) => router.push('/gallery') } className='text-white hover:opacity-60'>Gallery</p>
+                <p onClick={(e) => router.push('/pustaka') } className='text-white hover:opacity-60'>Pustaka</p>
             </div>
         </div>
     </nav>
