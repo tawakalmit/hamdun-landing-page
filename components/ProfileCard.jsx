fcf3db8106726a83fb127eb46bb9333861612343
tawakalmit <tawakal.mit@gmail.com>
2022-10-16 18:50:36 +0700

    card size

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function ProfileCard({image, description}) {
   return (
-    <div className="w-96 m-5 bg-[#ecf0f1] p-5 rounded-xl shadow-md">
+    <div className="w-80 md:w-96 m-5 bg-[#ecf0f1] p-5 rounded-xl shadow-md">
         <Image src={image} height={300} width={400} className="rounded-xl" alt='profile'/>
         <p className="text-lg mt-5">{description}</p>
     </div>
